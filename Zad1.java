7effd36523afabff4a2faf890e5805e9f9a2796d
Edi1201 <77607534+Edi1201@users.noreply.github.com>
2021-01-19 18:10:37 +0100

    Update Zad1.java

--- Zad1.java
+++ Zad1.java
@@ -1,14 +1,13 @@
 import java.util.Scanner;
 public class Zad1{
-	 public static void main(String args[]){
+	public static void main(String args[]){
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Podaj haslo:");
-        String ts_passUser = sc.nextLine();
-        final String ts_pass = "1234";
+		String ts_passUser = sc.nextLine();
+		final String ts_pass = "1234";
 		if(ts_passUser.equals(ts_pass))
-            System.out.println("Dobre haslo");
-        else
-            System.out.println("Zle haslo");
-        
-	 }
+			System.out.println("Dobre haslo");
+		else
+			System.out.println("Zle haslo");  
+	}
 }
